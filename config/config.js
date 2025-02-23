8f841054b4260ae4c16a39db27a8768bcbff00ea
silivestir <84616833+silivestir@users.noreply.github.com>
2025-02-23 14:23:34 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+/*
 require("dotenv").config()
 
 module.exports = {
@@ -9,6 +10,17 @@ module.exports = {
         port: process.env.DB_PORT || 5432,
         ssl: {
             rejectUnauthorized: false, // allows the connection with SSL required
+        }
+    }
+}*/module.exports = {
+    development: {
+        username: 'bgckrxlt',   // Username from URL
+        password: 'OQ2TG25MyY8LMSy-NXsSaY4pLGNxXmTy',  // Password from URL
+        database: 'bgckrxlt',   // Database name from URL
+        host: 'salt.db.elephantsql.com',  // Host from URL
+        port: 5432,  // Port from URL (Postgres default is 5432)
+        ssl: {
+            rejectUnauthorized: false,  // allows the connection with SSL required
         }
     }
 }
